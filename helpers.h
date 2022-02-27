22fa0e399a59151bb6f325683afc907dc054279a
Лучший <75216201+defini7@users.noreply.github.com>
2022-02-27 09:59:48 +0300

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -1,17 +1,9 @@
 #include "bmp.h"
+#include <math.h>
 
-// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
-
-// Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width]);
-
-// Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
-
-// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]);
-
 void threshold(int height, int width, RGBTRIPLE image[height][width]);
-
 void adaptive_threshold(int height, int width, RGBTRIPLE image[height][width]);
