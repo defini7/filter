495328c394416fa87cad9871b5408bda0775af7a
Лучший <75216201+defini7@users.noreply.github.com>
2022-02-27 09:59:23 +0300

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -1,120 +1,77 @@
 #include "helpers.h"
-#include <math.h>
 
-// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int h = 0; h < height; h++)
-    {
         for (int w = 0; w < width; w++)
         {
-            float i = image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed;
-            float j = i / 3;
-            int n = round(j);
+            int n = round((image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3);
 
             image[h][w].rgbtBlue = n;
             image[h][w].rgbtGreen = n;
             image[h][w].rgbtRed = n;
         }
-    }
-    return;
 }
 
-// Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int h = 0; h < height; h++)
-    {
         for (int w = 0; w < width; w++)
         {
-            float sepiaRed = .393 * image[h][w].rgbtRed + .769 * image[h][w].rgbtGreen + .189 * image[h][w].rgbtBlue;
-            float sepiaGreen = .349 * image[h][w].rgbtRed + .686 * image[h][w].rgbtGreen + .168 * image[h][w].rgbtBlue;
-            float sepiaBlue = .272 * image[h][w].rgbtRed + .534 * image[h][w].rgbtGreen + .131 * image[h][w].rgbtBlue;
 
-            int sr = round(sepiaRed);
-            int sg = round(sepiaGreen);
-            int sb = round(sepiaBlue);
+            int sr = round(.393 * image[h][w].rgbtRed + .769 * image[h][w].rgbtGreen + .189 * image[h][w].rgbtBlue);
+            int sg = round(.349 * image[h][w].rgbtRed + .686 * image[h][w].rgbtGreen + .168 * image[h][w].rgbtBlue);
+            int sb = round(.272 * image[h][w].rgbtRed + .534 * image[h][w].rgbtGreen + .131 * image[h][w].rgbtBlue);
 
-            if (sr > 255)
-            {
-                sr = 255;
-            }
-            if (sg > 255)
-            {
-                sg = 255;
-            }
-            if (sb > 255)
-            {
-                sb = 255;
-            }
-
-            image[h][w].rgbtRed = sr;
-            image[h][w].rgbtGreen = sg;
-            image[h][w].rgbtBlue = sb;
+            image[h][w].rgbtRed = (sr > 255) ? 255 : sr;
+            image[h][w].rgbtGreen = (sg > 255) ? 255 : sg;
+            image[h][w].rgbtBlue = (sb > 255) ? 255 : sb;
         }
-    }
-    return;
 }
 
-// Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
-    {
         for (int j = 0; j < (width / 2); j++)
         {
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - (j + 1)];
             image[i][width - (j + 1)] = temp;
         }
-    }
-    return;
 }
 
-// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp[height][width];
     for (int i = 0; i < height; i++)
-    {
         for (int j = 0; j < width; j++)
-        {
             temp[i][j] = image[i][j];
-        }
-    }
+
     for (int i = 0; i < height; i++)
-    {
         for (int j = 0; j < width; j++)
         {
-            float sum_red;
-            float sum_blue;
-            float sum_green;
-            int counter;
-            sum_red = sum_blue = sum_green = counter = 0;
+            float sum_red = 0, sum_blue = 0, sum_green = 0;
+            int counter = 0;
+
             for (int k = -1; k < 2; k++)
-            {
                 for (int l = -1; l < 2; l++)
                 {
                     if (i + k < 0 || i + k >= height)
-                    {
                         continue;
-                    }
+
                     if (j + l < 0 || j + l >= width)
-                    {
                         continue;
-                    }
+
                     sum_red += temp[i + k][j + l].rgbtRed;
                     sum_blue += temp[i + k][j + l].rgbtBlue;
                     sum_green += temp[i + k][j + l].rgbtGreen;
                     counter++;
                 }
-            }
+
             image[i][j].rgbtRed = round(sum_red / counter);
             image[i][j].rgbtGreen = round(sum_green / counter);
             image[i][j].rgbtBlue = round(sum_blue / counter);
         }
-    }
-    return;
 }
 
 void threshold(int height, int width, RGBTRIPLE image[height][width])
@@ -131,8 +88,6 @@ void threshold(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtGreen = value;
             image[i][j].rgbtBlue = value;
         }
-
-    return;
 }
 
 void adaptive_threshold(int height, int width, RGBTRIPLE image[height][width])
@@ -173,5 +128,4 @@ void adaptive_threshold(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtGreen = value;
             image[i][j].rgbtBlue = value;
         }
-    return;
 }
